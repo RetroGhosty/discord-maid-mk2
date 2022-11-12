04ee0d5018c7df01987ef044804085741925f45a
talipapa <54449218+RetroGhosty@users.noreply.github.com>
2022-11-13 02:13:44 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+import os
+from discord import app_commands
+
+
+class aclient(discord.Client):
+    def __init__(self):
+        super().__init__(intents=discord.Intents.default())
+        self.synced = False
+
+    async def on_ready(self):
+        await self.wait_until_ready()
+        if not self.synced:
+            await tree.sync()
+            self.synced = True
+        print(f'Logged in as {self.user}.')
+
+
+client = aclient()
+tree = app_commands.CommandTree(client)
+
+
+@tree.command(name='ping', description="test response")
+async def self(interaction: discord.Interaction, name: str):
+    await interaction.response.send_message(f'Hello {name}!')
+client.run('NjE3MDE1ODM3MDEzODM1Nzkx.GQbgRe.e6WdFx_28qLiYBB7eFX95Stjodvmv-qbhDD4cY')
